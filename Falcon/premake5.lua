4fc9357365667366a4a1d6e5b1d687beeff8fc11
ShantanuJamble <sjamble24@gmail.com>
2019-09-06 01:50:13 -0400

    Intermittent changes in premake file.

--- Falcon/premake5.lua
+++ Falcon/premake5.lua
@@ -37,8 +37,10 @@ printf("%s",LinkReleaseDirs.assimp)
 
 project "Falcon"
 	location "Falcon"
-	kind "ConsoleApp"
+	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
 
 	-- SETTING UP PATHS FOR BUILD DIRS--
@@ -85,13 +87,12 @@ project "Falcon"
 			"FL_PLATFORM_WINDOWS"
 		}
 
-		links 
+		--[=====[links 
 		{
 			"glfw3.lib",
 			"opengl32.lib";
-			"assimp-vc140-mt.lib"
 		}
-
+		--]=====]
 		nuget {'glm:0.9.9.500'}
 
 
@@ -101,6 +102,12 @@ project "Falcon"
 			defines "BUILD_DEBUG_MODE"
 			symbols "On"
      		
+			--[=====[links
+			{
+				"assimp-vc142-mtd.lib"
+			}
+			--]=====]
+
 			libdirs
 			{
 				"%{LinkDebugDirs.GLFW}",
@@ -115,12 +122,18 @@ project "Falcon"
 			defines "BUILD_RELEASE_MODE"
 			optimize "On"
 
+		--[=====[ 	links
+			{
+				"assimp-vc142-mt.lib"
+			}
+			--]=====]
 			libdirs
 			{
 				"%{LinkReleaseDirs.GLFW}",
 				"%{LinkReleaseDirs.boost}",
 				"%{LinkReleaseDirs.assimp}"
 			}
+
 
 	
 	--------SETTING UP THINGS FOR LINUX
@@ -145,9 +158,12 @@ project "Falcon"
             "GL",
             "X11",
 			"dl",
-			"pthread"
+			"pthread",
+			"assimp"
 		}
-	
+		
+
+
 
 
 	filter {"system:linux","configurations:Debug"}
@@ -185,11 +201,140 @@ project "Falcon"
 	--Setting up prebuild commands--
 	
 	filter{"configurations:Debug"}
-		assimp_abs_path = path.getabsolute(LinkDebugDirs["assimp"])
+		assimp_abs_path_deb = path.getabsolute(LinkDebugDirs["assimp"])
 	
-		postbuildcommands ('{COPY} "%{assimp_abs_path}" "%{cfg.targetdir}"')
+		postbuildcommands ('{COPY} "%{assimp_abs_path_deb}" "%{cfg.targetdir}"')
 
 	filter{"configurations:Release"}
-		assimp_abs_path = path.getabsolute(LinkReleaseDirs["assimp"])
+		assimp_abs_path_release = path.getabsolute(LinkReleaseDirs["assimp"])
 	
-		postbuildcommands ('{COPY} "%{assimp_abs_path}" "%{cfg.targetdir}"')
+		postbuildcommands ('{COPY} "%{assimp_abs_path_release}" "%{cfg.targetdir}"')
+
+
+--Game Project Config Setup--
+
+project "Game"
+	location "Game"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	-- SETTING UP PATHS FOR BUILD DIRS--
+	targetdir ("build/" .. outputdir .. "/bin/%{prj.name}")
+	objdir    ("build/" .. outputdir .. "/intermediates/%{prj.name}")
+
+	-- SETTING UP nuget PACKAGES--
+	nuget {'glm:0.9.9.500'}
+
+	files
+	{
+		"%{prj.name}/**.h",
+		"%{prj.name}/**.cpp",
+		"%{prj.name}/Shader/**.vert",
+		"%{prj.name}/Shader/**.frag"
+	}
+
+	includedirs
+	{
+		"${prj.name}/",
+		"Falcon/",
+		"Falcon/System",
+		"%{IncludeDirs.vendor}",
+		"%{IncludeDirs.glad}",
+		"%{IncludeDirs.GLFW}",
+		"%{IncludeDirs.KHR}",
+		"%{IncludeDirs.boost}",
+		"%{IncludeDirs.spdlog}",
+		"%{IncludeDirs.assimp}"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"FL_PLATFORM_WINDOWS"
+
+		}
+
+		links
+		{
+			"Falcon",
+			"glfw3.lib",
+			"opengl32.lib";
+			
+		}
+
+	filter "system:linux"
+		systemversion "latest"
+
+		defines
+		{
+			"FL_PLATFORM_LINUX"
+		}
+
+		links 
+		{
+			"Falcon",
+			"Xrandr",
+	        "Xi",
+			"glfw3",
+        	"GLEW",
+            "GLU",
+            "GL",
+            "X11",
+			"dl",
+			"pthread",
+			"assimp"
+		}
+		
+
+
+	filter "configurations:Debug"
+		defines "FL_GAME_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+		libdirs
+		{
+			"%{LinkDebugDirs.GLFW}",
+			"%{LinkDebugDirs.boost}",
+			"%{LinkDebugDirs.assimp}",
+		}
+
+		links
+		{
+			"assimp-vc142-mtd.lib"
+		}
+
+
+	filter "configurations:Release"
+		defines "FL_GAME_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+			"assimp-vc142-mt.lib"
+		}
+		libdirs
+		{
+			"%{LinkReleaseDirs.GLFW}",
+			"%{LinkReleaseDirs.boost}",
+			"%{LinkReleaseDirs.assimp}",
+		}
+
+
+	filter{"configurations:Debug"}
+		falcon_path_deb = "build/" .. outputdir .. "/bin/Falcon"
+		falcon_abs_path_deb = path.getabsolute(falcon_path_deb)
+	
+		postbuildcommands ('{COPY} "%{falcon_abs_path_deb}" "%{cfg.targetdir}"')
+
+	filter{"configurations:Release"}
+
+		falcon_path_release = "build/" .. outputdir .. "/bin/Falcon"
+		falcon_abs_path_release = path.getabsolute(falcon_path_release)
+
+		postbuildcommands ('{COPY} "%{falcon_abs_path_release}" "%{cfg.targetdir}"')
