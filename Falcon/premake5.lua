19760082f9adfdb82ad6607b493de3a6193cfc23
Shantanu Jamble <sjamble24@gmail.com>
2019-09-13 15:20:56 -0400

    Updated project namings to Falcon from GraphicsEngine.

--- Falcon/premake5.lua
+++ Falcon/premake5.lua
@@ -1,4 +1,4 @@
-workspace "GraphicsEngine"
+workspace "Falcon"
 	architecture "x64"
 
 	configurations
@@ -11,32 +11,32 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 IncludeDirs = {}
 
-IncludeDirs["vendor"] = "GraphicsEngine/vendor/include/";
-IncludeDirs["spdlog"] = "GraphicsEngine/vendor/include/spdlog";
-IncludeDirs["GLFW"]   = "GraphicsEngine/vendor/include/glfw";
-IncludeDirs["glad"]   = "GraphicsEngine/vendor/include/glad";
-IncludeDirs["assimp"] = "GraphicsEngine/vendor/include/assimp";
-IncludeDirs["KHR"]    = "GraphicsEngine/vendor/include/KHR";
-IncludeDirs["boost"]  = "GraphicsEngine/vendor/include/boost";
+IncludeDirs["vendor"] = "Falcon/vendor/include/";
+IncludeDirs["spdlog"] = "Falcon/vendor/include/spdlog";
+IncludeDirs["GLFW"]   = "Falcon/vendor/include/glfw";
+IncludeDirs["glad"]   = "Falcon/vendor/include/glad";
+IncludeDirs["assimp"] = "Falcon/vendor/include/assimp";
+IncludeDirs["KHR"]    = "Falcon/vendor/include/KHR";
+IncludeDirs["boost"]  = "Falcon/vendor/include/boost";
 
 
 LinkDebugDirs = {}
-LinkDebugDirs["assimp"] = "GraphicsEngine/vendor/libs/assimp/Debug"
-LinkDebugDirs["boost"]  = "GraphicsEngine/vendor/libs/boost"
-LinkDebugDirs["GLFW"]   = "GraphicsEngine/vendor/libs/GLFW/Debug"
+LinkDebugDirs["assimp"] = "Falcon/vendor/libs/assimp/Debug"
+LinkDebugDirs["boost"]  = "Falcon/vendor/libs/boost"
+LinkDebugDirs["GLFW"]   = "Falcon/vendor/libs/GLFW/Debug"
 
 LinkReleaseDirs = {}
-LinkReleaseDirs["assimp"] = "GraphicsEngine/vendor/libs/assimp/Release"
-LinkReleaseDirs["boost"]  = "GraphicsEngine/vendor/libs/boost"
-LinkReleaseDirs["GLFW"]   = "GraphicsEngine/vendor/libs/GLFW/Release"
+LinkReleaseDirs["assimp"] = "Falcon/vendor/libs/assimp/Release"
+LinkReleaseDirs["boost"]  = "Falcon/vendor/libs/boost"
+LinkReleaseDirs["GLFW"]   = "Falcon/vendor/libs/GLFW/Release"
 
 
 
 printf("%s",LinkReleaseDirs.assimp)
 
 
-project "GraphicsEngine"
-	location "GraphicsEngine"
+project "Falcon"
+	location "Falcon"
 	kind "ConsoleApp"
 	language "C++"
 
