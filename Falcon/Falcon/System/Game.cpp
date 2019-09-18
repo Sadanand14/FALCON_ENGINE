2fbe85f6cbf733333d867b86cbe3ec36d4a28957
Shantanu Jamble <sjamble24@gmail.com>
2019-09-18 10:02:03 -0400

    Demo for the test class with fnew

--- Falcon/Falcon/System/Game.cpp
+++ Falcon/Falcon/System/Game.cpp
@@ -1,12 +1,29 @@
 #include "Game.h"
 #include "Log.h"
 #include <iostream>
+#include <Memory/fmemory.h>
+
+class Test
+{
+public:
+	Test()
+	{
+		FL_ENGINE_INFO("using default constor");
+	}
+	Test(int a)
+	{
+		FL_ENGINE_INFO("Using Test constructor with 1 argument");
+	}
+};
 
 int main()
 {
 	Log::Init();
+	fmemory::MemoryManagerInit();
+
 	WindowClass* window1 = new WindowClass("FalconEngine", 1280, 720);
 	InputReceiver* input = new InputReceiver(window1);
+	Test* test = fnew<Test>(10);
 
 	while(!window1->WindowCloseStatus())
 	{
@@ -14,6 +31,7 @@ int main()
 	}
 
 	delete input;
-	delete window1;
+	fdelete<WindowClass>(window1);
+	fmemory::MeoryManagerShutDown();
 	return 0;
 }
