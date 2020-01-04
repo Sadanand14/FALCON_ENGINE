dc83c2c3549aed67062a41bb6d07d8d6566191f5
Sadanand Naik <san2889@rit.edu>
2020-01-04 04:32:33 -0500

    fixed per frame allocation for physics

--- Falcon/Falcon/Physics/PhysicsSystem.cpp
+++ Falcon/Falcon/Physics/PhysicsSystem.cpp
@@ -14,20 +14,20 @@ void PhysicsSystem::update(float dt, boost::container::vector<Entity*, fmemory::
 	glm::vec3 pre_sim_pos, lerp_pos,  post_sim_pos;
 	glm::quat pre_sim_rot,lerp_rot, post_sim_rot;
 	const physx::PxRigidActor* actor;
-	boost::container::vector<Entity*, fmemory::StackSTLAllocator<Entity*>> entitySet = *entity;
+	boost::container::vector<Entity*, fmemory::StackSTLAllocator<Entity*>>* entitySet = entity;
 	for (size_t i = 0; i < entity_count; ++i)
 	{
 		//Update logic
-		actor = entitySet[i]->GetComponent<PhysicsComponent>()->GetActor();;
+		actor = entitySet->at(i)->GetComponent<PhysicsComponent>()->GetActor();;
 		PXMathUtils::PxVec3toVec3(actor->getGlobalPose().p, post_sim_pos);
 		PXMathUtils::PxQuattoQuat(actor->getGlobalPose().q, post_sim_rot);
-		pre_sim_pos = entitySet[i]->GetTransform()->GetPosition();
-		pre_sim_rot = entitySet[i]->GetTransform()->GetRotation();
-		FL_ENGINE_INFO("New POS= {0},{1},{2}", post_sim_pos.x, post_sim_pos.y, post_sim_pos.z);
+		pre_sim_pos = entitySet->at(i)->GetTransform()->GetPosition();
+		pre_sim_rot = entitySet->at(i)->GetTransform()->GetRotation();
+		//FL_ENGINE_INFO("New POS= {0},{1},{2}", post_sim_pos.x, post_sim_pos.y, post_sim_pos.z);
 		lerp_pos = PXMathUtils::lerp(pre_sim_pos, post_sim_pos, dt);
 		lerp_rot = glm::slerp(pre_sim_rot, post_sim_rot, dt);
 
-		entitySet[i]->GetTransform()->SetPosition(lerp_pos);
-		entitySet[i]->GetTransform()->SetRotation(lerp_rot);
+		entitySet->at(i)->GetTransform()->SetPosition(lerp_pos);
+		entitySet->at(i)->GetTransform()->SetRotation(lerp_rot);
 	}
 }
