88e50e8b937559b72ae6f3898d685c9b00c9e258
Alabhya Maheshwari (RIT Student) <am7630@ad.rit.edu>
2020-01-15 10:04:58 -0500

    Revert "Revert "fixed assetmanagement completely to assetManager""
    This reverts commit bdd3cc61dc0afc29e6c7874e21b23d7f5a718177.

--- Falcon/Falcon/Core/EntityManager.cpp
+++ Falcon/Falcon/Core/EntityManager.cpp
@@ -3,65 +3,11 @@
 boost::unordered_map<std::string, Mesh*> EntityManager::m_meshes;
 boost::unordered_map<std::string, Material*> EntityManager::m_materials;
 
-/**
- * Loads a mesh into the scene
- * @param meshPath - The path to the mesh json file
- */
-void EntityManager::LoadMesh(const std::string& meshPath)
-{
-	//Get file data
-	char* json = nullptr;
-	int32_t size;
-	std::ifstream jsonFile(meshPath, std::ios::in | std::ios::ate | std::ios::binary);
-	if (jsonFile.is_open()) {
-		size = jsonFile.tellg();
-		jsonFile.seekg(std::ios::beg);
-		json = fmemory::fnew_arr<char>(size + 1);
-		jsonFile.read(json, size);
-		json[size] = 0;
-		jsonFile.close();
-	}
-
-	//Start json doc
-	rapidjson::Document doc;
-	doc.Parse(json);
-	fmemory::fdelete<char>(json);
-
-	//Set the mesh path
-	std::string path = doc["path"].GetString();
-	Mesh* m = AssetManager::LoadModel(path);
-	m->SetJsonPath(meshPath);
-	m->SetPath(path);
-
-	//Check if the material already exists
-	auto mat = m_materials.find(doc["material"].GetString());
-
-	//Load material if it doesn't exist
-	if (mat == m_materials.end())
-		LoadMaterial(doc["material"].GetString());
-
-	//Set material and alloc the amount of instances for the matrices
-	m->SetMaterial(m_materials[doc["material"].GetString()]);
-	m->PreallocMatrixAmount(doc["instances"].GetInt());
-	m_meshes[meshPath] = m;
-}
-
-/**
- * Loads a material into the scene
- * @param matPath - The path to the material json file
- */
-void EntityManager::LoadMaterial(const std::string& matPath)
-{
-	Material* mat = AssetManager::LoadMaterial(matPath);
-	mat->SetPath(matPath);
-	m_materials[matPath] = mat;
-}
 
 /**
  * Loads a scene
  * @param sceneFilePath - The path to the scene file
  */
-
 
 Entity* EntityManager::CreateEntity(const char* objTemplate, glm::vec3 pos, glm::quat rot, glm::vec3 scale)
 {
@@ -104,14 +50,14 @@ Entity* EntityManager::CreateEntity(const char* objTemplate, glm::vec3 pos, glm:
 			//Get mesh
 			const rapidjson::Value& mesh = doc["renderComponent"]["mesh"];
 
-			auto m = m_meshes.find(mesh.GetString());
+			/*auto m = m_meshes.find(mesh.GetString());
 
 			if (m == m_meshes.end())
 				LoadMesh(mesh.GetString());
 			RenderComponent* rc = newEntity->GetComponent<RenderComponent>();
-			rc->m_mesh =	m_meshes[mesh.GetString()];
-			/*RenderComponent* rc = newEntity->GetComponent<RenderComponent>();
-			rc->m_mesh = AssetManager::GetMesh(mesh.GetString());*/
+			rc->m_mesh =	m_meshes[mesh.GetString()];*/
+			RenderComponent* rc = newEntity->GetComponent<RenderComponent>();
+			rc->m_mesh = AssetManager::GetMesh(mesh.GetString());
 			rc->CalculateBounds();
 		}
 
@@ -163,8 +109,8 @@ Entity* EntityManager::CreateEntity(const char* objTemplate, glm::vec3 pos, glm:
 
 			//Get mesh
 			const rapidjson::Value& mat = doc["particleEmitterComponent"]["material"];
-			//AssetManager::GetMaterial(mat.GetString());
-			LoadMaterial(mat.GetString());
+			AssetManager::GetMaterial(mat.GetString());
+			//LoadMaterial(mat.GetString());
 			particleComp->m_particle = fmemory::fnew<Particle>();
 			particleComp->m_particle->Setup();
 			particleComp->m_particle->PreallocParticleDataAmount(particleComp->m_particleBuffer.capacity());
